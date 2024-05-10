c578604e2f5910deb7e2cb441f2371255174a94a
jws <69137791+sinbhs@users.noreply.github.com>
2024-05-10 10:54:47 +0900

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
 		<h2>Welcome</h2>
 		<p><span className="inline-block align-top">Hello, WEB! </span><img className="inline-block align-top" src="/hand.png" alt="파일이미지" style={{width: 30 + 'px'}}></img></p>
 
+		<p><code>npx json-server --port 9999 --watch db.json</code></p>
+		<p><code>npm run dev</code></p>
 	</>
   );
 }
